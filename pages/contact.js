081f443299bc182ade7cac2cf4b5b593e7414706
Soumyadip <soumyadipgantait2003@gmail.com>
2023-05-30 21:37:54 +0530

    still solving

--- pages/contact.js
+++ pages/contact.js
@@ -89,7 +89,7 @@ console.log(response)
           Password
         </label>
         <input  value={password}  onChange={handleChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name='password' id="grid-password"  placeholder="******************"/>
-        <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+        <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you had like</p>
       </div>
     </div>
     <div className="flex flex-wrap -mx-3 mb-2">
